c7876dbceb32b700d80e22c028005ce74ac415b4
Alex Merritt <merritt.alex@gatech.edu>
2011-11-09 00:36:30 -0500

    Updated comments in the API.

--- shmgrp/include/shmgrp.h
+++ shmgrp/include/shmgrp.h
@@ -4,6 +4,14 @@
  * @date 2011-11-06
  * @brief TODO
  *
+ * ALL functions return 0 (zero) on success and less than 0 if not successful.
+ * Values less than zero are actually negative errno codes to indicate what went
+ * wrong. But they're not clean right now, as I expose low-level errno codes
+ * directly returned from system calls within the library as the return code of
+ * an API routine. However, you can be pretty sure that if you get
+ * 		-EINVAL, -EEXIST, or -ENOENT
+ * it means you are not passing valid arguments.
+ *
  * TODO LIST ASSUMPTIONS, LIMITATIONS, RESTRICTIONS HERE.
  */
 
@@ -14,107 +22,169 @@
 
 /*-------------------------------------- DEFINITIONS -------------------------*/
 
-//! This defines the maximum length usable for a group key. It must remain
-//! short, as it is directly used in many places within the library where other
-//! character limitations are in place.
+/**
+ * Maximum number of characters allowable in a group key. Group keys should
+ * contain printable ASCII characters minus the following: / and space
+ */
 #define SHMGRP_MAX_KEY_LEN	128
 
+/**
+ * ID type for a shared memory region. Region IDs are unique within each
+ * group-member tuple.
+ */
 typedef int shmgrp_region_id;
 
-struct shm_region
+/**
+ * Structure describing a mapped region.
+ */
+struct shmgrp_region
 {
-	size_t size;
-	void *addr;
 	shmgrp_region_id id;
+	void *addr;
+	size_t size;
 };
 
+/**
+ * Event types group_callback will be given when invoked by the library.
+ */
 typedef enum { MEMBERSHIP_JOIN = 1, MEMBERSHIP_LEAVE } group_event;
+
+/**
+ * Event types shm_callback will be given when invoked by the library.
+ */
 typedef enum { SHM_CREATE_REGION = 1, SHM_REMOVE_REGION } shm_event;
 
 /**
- * TODO
- * maybe add a third arg identifying the group id? or allow a user-defined value
- * to be passed in?
+ * Callback routine which must be defined by the application, and associated
+ * with a specific group key upon creation of said group.
  *
- * If one function is registered for multiple groups, it will need to be
- * thread-safe as multiple threads in the library will be calling into it.
+ * Each group created should have a separate function defined for the callback.
+ * The routine will be invoked when other processes join or leave the group. The
+ * PID may be used to differentiate among members in the group.
+ *
+ * This function must be thread-safe.
  */
 typedef void (*group_callback)(group_event, pid_t);
 
+/**
+ * Callback routine which must be defined by the application, and associated
+ * with a specific member of a group when that member has been granted access to
+ * join the group.
+ *
+ * One function may be used for all members of a group, as the PID is a provided
+ * parameter. The region ID parameter represents a region that has already been
+ * mapped in and its associated virtual memory region written to or read from.
+ * This function is invoked when a member process calls shmgrp_mkreg.
+ *
+ * This function must be thread-safe.
+ */
 typedef void (*shm_callback)(shm_event, pid_t, shmgrp_region_id);
 
 /*-------------------------------------- COMMON FUNCTIONS --------------------*/
 
 /**
- * TODO
+ * Initialize the shmgrp library state.
+ *
+ * This must happen once and before any of the below functions are called.
  */
 int shmgrp_init(void);
 
 /**
- * TODO
+ * Deallocate the shmgrp library state.
+ *
+ * This must happen last.
  */
 int shmgrp_tini(void);
 
 /**
- * TODO
+ * Return information associated with the given region.
+ *
+ * Valid region IDs come from arguments passed into invocations of the
+ * shm_callback routine. Do not call munmap on the addresses returned by this
+ * function, or I will cut your face off.
+ *
+ * @param reg	Output parameter where region information will be written.
  */
-void* shmgrp_addr(shmgrp_region_id id);
+int shmgrp_region(shmgrp_region_id id, shmgrp_region *reg);
 
 /**
- * TODO
+ * Convert the given group_event to a human-readable string for printing.
  */
 const char * shmgrp_memb_str(group_event e);
 
 /*-------------------------------------- LEADER FUNCTIONS --------------------*/
 
 /**
- * TODO
+ * Open a new group for members to join.
  */
 int shmgrp_open(const char *key, group_callback func);
 
 /**
- * TODO
+ * Close an existing group and prevent new members from joining.
+ *
+ * FIXME This will probably break if members still exist when it is called.
+ * Ensure they all leave before you shut down.
  */
 int shmgrp_close(const char *key);
 
 /**
- * This must be called with each invocation of the callback registered with the
- * creation of a group. It finalizes the addition of a new member to the group,
- * identified by the PID passed as an argument to the callback (new members will
- * spin indefinitely inside join() if this is not called).
+ * Allow a member to join a group.
  *
- * The callback will be invoked every time a member process requests to create
- * or destroy a shared memory segment. If the same function is registered more
- * than once, it will need to be thread-safe.
+ * This function must only be called from within the group_callback routine
+ * registered with the creation of a group, and only when the event signifies a
+ * new member joining. You must provide a second-level callback routine that
+ * will be invoked by accepted members when they create and destroy shared
+ * memory regions.
  */
 int shmgrp_establish_member(const char *key, pid_t pid, shm_callback func);
 
 /*-------------------------------------- MEMBER FUNCTIONS --------------------*/
 
 /**
- * TODO
+ * Request to join a group.
+ *
+ * This will spin indefinitely if a leader decides not to permit you. A member
+ * may not join a group more than once, but it may join and be a member of
+ * multiple groups simultaneously.
  */
 int shmgrp_join(const char *key);
 
 /**
- * TODO
+ * Leave a group.
+ *
+ * FIXME You should remove all memory regions before doing this. I don't know
+ * what will happen if you do not.
  */
 int shmgrp_leave(const char *key);
 
 /**
- * TODO
- * add some size argument
+ * Create a shared memory region with your group leader.
  *
- * the value of the ID returned is common between both the member and the
- * leader, but is not unique across members of the same group, nor across
- * groups. in other words, if you were to communicate this ID out-of-band to the
- * leader or member involved, they would be able to identify the same physical
- * region of memory.
+ * A member may create shared memory regions only with groups it has been
+ * permitted to join. Any number of regions may be created. Each time a region
+ * is created, the leader is immediately notified and has that region mapped in
+ * its address space. The leader will also know the PID of the member invoking
+ * this function. The leader's shm_callback is invoked when a member calls this
+ * function.
+ *
+ * The region ID returned is unique to only the member and the group leader. In
+ * other words, it is not unique across groups. However, the value of the ID is
+ * common between a member and leader, that is, if a member communicates the
+ * region ID out-of-band to the leader (or vice versa) either side can use it to
+ * locate the same physical memory mapping.
+ *
+ * Obviously the key must associate with a group the member has been admitted
+ * to.
  */
 int shmgrp_mkreg(const char *key, size_t size, shmgrp_region_id *id);
 
 /**
- * TODO
+ * Destroy a shared memory mapping.
+ *
+ * A member and leader should agree to stop using a shared memory region before
+ * a member invokes this function. This will fail if the member calling this
+ * function has not joined the group represented by the key, or if the ID does
+ * not represent any known region.
  */
 int shmgrp_rmreg(const char *key, shmgrp_region_id id);
 
