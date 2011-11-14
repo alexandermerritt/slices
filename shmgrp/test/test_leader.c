591a36239ed8600d33c0f256c8106972dcc24045
Alex Merritt <merritt.alex@gatech.edu>
2011-11-13 19:04:21 -0500

    Added more flexibility to shmgrp test code.
    Also updated TODO file with bugs.

--- shmgrp/test/test_leader.c
+++ shmgrp/test/test_leader.c
@@ -11,10 +11,24 @@
 #include <shmgrp.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+
+// Locking for printf
+pthread_mutex_t lock;
+#define printf_locked(fmt, args...)		\
+	do {								\
+		pthread_mutex_lock(&lock);		\
+		printf(fmt, ##args);			\
+		pthread_mutex_unlock(&lock);	\
+	} while(0)
+#define fprintf_locked(err, fmt, args...)	\
+	do {									\
+		pthread_mutex_lock(&lock);			\
+		fprintf(err, fmt, ##args);			\
+		pthread_mutex_unlock(&lock);		\
+	} while(0)
 
 #define GRPKEY1	"cudarpc"
-pid_t pid;
-unsigned int value = 0xf005ba11;
 
 void cudarpc_shm_callback(shm_event e, pid_t pid, shmgrp_region_id id)
 {
@@ -26,18 +40,15 @@ void cudarpc_shm_callback(shm_event e, pid_t pid, shmgrp_region_id id)
 		case SHM_CREATE_REGION:
 			err = shmgrp_member_region(GRPKEY1, pid, id, &region);
 			if (err < 0) {
-				fprintf(stderr, "oopsie, PID %d created an shm %d,"
-						" but we can't find it\n", pid, id);
+				fprintf_locked(stderr, "shm create: cannot find id %d\n", id);
 			} else {
-				printf("oh boy, PID %d wants to talk with us"
-						" on region %d @ %p for %d bytes\n",
+				printf_locked("shm create: pid %d made region %d @ %p for %d bytes\n",
 						pid, id, region.addr, region.size);
-				printf("i'll tell it to play foosball\n");
-				*((unsigned int *)region.addr) = value;
+				*((unsigned int *)region.addr) = 0xf005ba11;
 			}
 			break;
 		case SHM_REMOVE_REGION:
-			printf("shucks, PID %d is closing region (%d)\n", pid, id);
+			printf_locked("shm rem: pid %d closed region %d\n", pid, id);
 			break;
 	}
 }
@@ -48,14 +59,20 @@ void cudarpc_callback(group_event e, pid_t pid)
 
 	if (e == MEMBERSHIP_JOIN) {
 		err = shmgrp_establish_member(GRPKEY1, pid, cudarpc_shm_callback);
-		if (err < 0)
-			fprintf(stderr, "oops, PID %d can't be a member: %s\n",
-					pid, strerror(-(err)));
-		printf("hello hello to member PID %d\n", pid);
+		if (err < 0) {
+			fprintf_locked(stderr, "grp join: pid %d requested,"
+					" but can't establish: %s\n", pid, strerror(-(err)));
+		}
+		printf_locked("grp join: member pid %d successful\n", pid);
 		// implement application-level setup here
 	}
 	else if (e == MEMBERSHIP_LEAVE) {
-		printf("bye bye to member PID %d\n", pid);
+		err = shmgrp_destroy_member(GRPKEY1, pid);
+		if (err < 0) {
+			fprintf_locked(stderr, "grp leave: pid %d requested,"
+					" but can't destroy: %s\n", pid, strerror(-(err)));
+		}
+		printf_locked("grp leave: member PID %d\n", pid);
 		// implement application-level cleanup here
 	}
 }
@@ -64,40 +81,44 @@ int main(void)
 {
 	int err, sleeptime=30;
 
+	pthread_mutex_init(&lock, NULL);
+
 	err = shmgrp_init();
 	if (err < 0) {
-		fprintf(stderr, "Error: shmgrp_init: %s\n", strerror(-err));
+		fprintf_locked(stderr, "Error: shmgrp_init: %s\n", strerror(-err));
 		return -1;
 	}
 
-	printf("Opening group %s\n", GRPKEY1);
+	printf_locked("Opening group %s\n", GRPKEY1);
 	err = shmgrp_open(GRPKEY1, cudarpc_callback, false);
 	if (err < 0) {
-		fprintf(stderr, "Error: shmgrp_open(%s): %s\n",
+		fprintf_locked(stderr, "Error: shmgrp_open(%s): %s\n",
 				GRPKEY1, strerror(-err));
 		return -1;
 	}
 
-	printf("Sleeping for %d seconds."
+	printf_locked("Sleeping for %d seconds."
 			" You must execute the member now.\n", sleeptime);
 
 	while(sleeptime-- > 0) {
-		if (sleeptime %2 == 0) // print every two seconds
-			printf("Sleeping... %d\n", sleeptime), fflush(stdout);
+		if (sleeptime %2 == 0) { // print every two seconds
+			printf_locked("Sleeping... %d\n", sleeptime);
+			fflush(stdout);
+		}
 		sleep(1);
 	}
 
-	printf("Closing group %s\n", GRPKEY1);
+	printf_locked("Closing group %s\n", GRPKEY1);
 	err = shmgrp_close(GRPKEY1);
 	if (err < 0) {
-		fprintf(stderr, "Error: shmgrp_close(%s): %s\n",
+		fprintf_locked(stderr, "Error: shmgrp_close(%s): %s\n",
 				GRPKEY1, strerror(-err));
 		return -1;
 	}
 
 	err = shmgrp_tini();
 	if (err < 0) {
-		fprintf(stderr, "Error: shmgrp_tini: %s\n", strerror(-err));
+		fprintf_locked(stderr, "Error: shmgrp_tini: %s\n", strerror(-err));
 		return -1;
 	}
 }
