33793cd962a6098a9cf7f8a4550868e58057642c
Alex Merritt <merritt.alex@gatech.edu>
2011-11-29 15:50:54 -0500

    Script to clean system state after unclean exit.
    If the assembly runtime or a sink terminates without cleaning up, or does not
    terminate, run this script to perform that cleanup.
    
    If a process ends up not dying after use of the script, remote it with
    	kill -s SIGKILL pid

---
+++ backend/purge-system-resources.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Author: Alex Merritt, merritt.alex@gatech.edu
+# This file is meant to clean system resources and files from an unclean
+# termination of the assembly runtime and associated sink processes.
+
+set -e
+set -u
+
+# Double-check with the user, just in case.
+echo ""
+echo "This script will purge resources and lingering processes"
+echo "allocated by the assembly runtime which may not have been"
+echo "removed due to an unclean termination."
+echo ""
+echo -n "Are you sure? [y/N] "
+
+read confirm
+
+if [[ ! "$confirm" == "y" ]]
+then
+	echo "Not confirmed."
+	exit 0
+fi
+
+# Kill all runtime processes. There may be more than one lingering around.
+# SIGINT is the signal these processes explicitly listen for.
+RUNTIME_PIDS=`ps aux | grep runtime | grep -v grep | awk '{print $2}' | tr "\n" " "`
+for RUNTIME_PID in $RUNTIME_PIDS
+do
+	echo Killing runtime $RUNTIME_PID
+	kill -s SIGINT $RUNTIME_PID
+	sleep 1 # Give the process time to cleanup.
+done
+
+# Kill all sink processes.
+# SIGTERM is the signal these processes explicitly listen for.
+SINK_PIDS=`ps aux | grep sink | grep -v grep | awk '{print $2}' | tr "\n" " "`
+for SINK_PID in $SINK_PIDS
+do
+	echo Killing sink $SINK_PID
+	kill -s SIGTERM $SINK_PID
+	sleep 1 # Give the process time to cleanup.
+done
+
+# Remove registration and shm files.
+rm -rf -v /tmp/shmgrp/
+rm -f -v /dev/shm/shmgrp-*
+rm -f -v /dev/shm/assm-export-*
+
+echo ""
+echo "Done."
