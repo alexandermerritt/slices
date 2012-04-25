c2787f9cd64e48bd21233fc5e761ae6ac2e4336b
Alex Merritt <merritt.alex@gatech.edu>
2012-04-25 00:13:37 -0400

    updated comment in shadowfax script

--- scripts/shadowfax.py
+++ scripts/shadowfax.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 
-# Functions to help control Shadowfax runtime.
+# Functions to help control Shadowfax runtime. You will need to edit this for
+# the correct location of the source code and the network type to build with.
+# To be used, this script should be placed in a directory that the environment
+# variable PYTHONPATH points to. Or you can import it directly using a quoted
+# relative path.
 
 import os
 import time
@@ -12,7 +16,7 @@ SHADOWFAX_CODE_DIR = HOME + '/code/shadowfax.git'
 HOSTNAME = os.environ['HOSTNAME']
 
 def build(dns):
-	install = [HOME + '/bin/scons', '-Q', '-C', SHADOWFAX_CODE_DIR, 'network=sdp']
+	install = [HOME + '/bin/scons', '-Q', '-C', SHADOWFAX_CODE_DIR, 'network=eth']
 	ssh = ['ssh', dns]
 	sp.call(ssh + install)
 
