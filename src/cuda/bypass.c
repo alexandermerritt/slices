6c210dad458e03b81f38d2315a4d22fd82129ac8
Alexander Merritt <merritt.alex@gatech.edu>
2013-03-24 18:59:53 -0400

    bypass code uses dlopen instead of RTLD_NEXT
    so much pain with library order and symbol resolving
    
    apparently RTLD_NEXT only goes to the 'next' symbol given the position of the
    code calling dlsym, and cannot iterate across definitions it finds. also,
    RTLD_NEXT when used from a code which doesn't have a definition of the symbol
    will just return the first symbol found. So you have to use RTLD_NEXT from
    within a library which provides an implementation.
    
    This fix just does a direct dlopen on libcudart.so and pulls in the symbols
    directly (the cuda functions). If already opened, dlopen will just return the
    handle to the library file.

--- src/cuda/bypass.c
+++ src/cuda/bypass.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <debug.h>
 
 // Globals
 
@@ -18,55 +19,61 @@ struct bypass bypass;
 
 void fill_bypass(struct bypass *bypass)
 {
-	// damn compiler complains about void* casting (icc, not gcc)
-	bypass->cudaBindTexture            =  (fn_cudaBindTexture)         	dlsym(RTLD_NEXT,  "cudaBindTexture");
-	bypass->cudaBindTextureToArray     =  (fn_cudaBindTextureToArray)  	dlsym(RTLD_NEXT,  "cudaBindTextureToArray");
-	bypass->cudaConfigureCall          =  (fn_cudaConfigureCall)       	dlsym(RTLD_NEXT,  "cudaConfigureCall");
-	bypass->cudaCreateChannelDesc      =  (fn_cudaCreateChannelDesc)   	dlsym(RTLD_NEXT,  "cudaCreateChannelDesc");
-	bypass->cudaDriverGetVersion       =  (fn_cudaDriverGetVersion)    	dlsym(RTLD_NEXT,  "cudaDriverGetVersion");
-	bypass->cudaFreeArray              =  (fn_cudaFreeArray)           	dlsym(RTLD_NEXT,  "cudaFreeArray");
-	bypass->cudaFree                   =  (fn_cudaFree)                	dlsym(RTLD_NEXT,  "cudaFree");
-	bypass->cudaFreeHost               =  (fn_cudaFreeHost)            	dlsym(RTLD_NEXT,  "cudaFreeHost");
-	bypass->cudaFuncGetAttributes      =  (fn_cudaFuncGetAttributes)   	dlsym(RTLD_NEXT,  "cudaFuncGetAttributes");
-	bypass->cudaGetDeviceCount         =  (fn_cudaGetDeviceCount)      	dlsym(RTLD_NEXT,  "cudaGetDeviceCount");
-	bypass->cudaGetDevice              =  (fn_cudaGetDevice)           	dlsym(RTLD_NEXT,  "cudaGetDevice");
-	bypass->cudaGetDeviceProperties    =  (fn_cudaGetDeviceProperties) 	dlsym(RTLD_NEXT,  "cudaGetDeviceProperties");
-	bypass->cudaGetErrorString         =  (fn_cudaGetErrorString)      	dlsym(RTLD_NEXT,  "cudaGetErrorString");
-	bypass->cudaGetLastError           =  (fn_cudaGetLastError)        	dlsym(RTLD_NEXT,  "cudaGetLastError");
-	bypass->cudaGetTextureReference    =  (fn_cudaGetTextureReference) 	dlsym(RTLD_NEXT,  "cudaGetTextureReference");
-	bypass->cudaHostAlloc              =  (fn_cudaHostAlloc)           	dlsym(RTLD_NEXT,  "cudaHostAlloc");
-	bypass->cudaLaunch                 =  (fn_cudaLaunch)              	dlsym(RTLD_NEXT,  "cudaLaunch");
-	bypass->cudaMallocArray            =  (fn_cudaMallocArray)         	dlsym(RTLD_NEXT,  "cudaMallocArray");
-	bypass->cudaMalloc                 =  (fn_cudaMalloc)              	dlsym(RTLD_NEXT,  "cudaMalloc");
-	bypass->cudaMallocPitch            =  (fn_cudaMallocPitch)         	dlsym(RTLD_NEXT,  "cudaMallocPitch");
-	bypass->cudaMemcpyAsync            =  (fn_cudaMemcpyAsync)         	dlsym(RTLD_NEXT,  "cudaMemcpyAsync");
-	bypass->cudaMemcpy                 =  (fn_cudaMemcpy)              	dlsym(RTLD_NEXT,  "cudaMemcpy");
-	bypass->cudaMemcpyFromSymbol       =  (fn_cudaMemcpyFromSymbol)    	dlsym(RTLD_NEXT,  "cudaMemcpyFromSymbol");
-	bypass->cudaMemcpyToArray          =  (fn_cudaMemcpyToArray)       	dlsym(RTLD_NEXT,  "cudaMemcpyToArray");
-	bypass->cudaMemcpyToSymbolAsync    =  (fn_cudaMemcpyToSymbolAsync) 	dlsym(RTLD_NEXT,  "cudaMemcpyToSymbolAsync");
-	bypass->cudaMemcpyToSymbol         =  (fn_cudaMemcpyToSymbol)      	dlsym(RTLD_NEXT,  "cudaMemcpyToSymbol");
-	bypass->cudaMemGetInfo             =  (fn_cudaMemGetInfo)          	dlsym(RTLD_NEXT,  "cudaMemGetInfo");
-	bypass->cudaMemset                 =  (fn_cudaMemset)              	dlsym(RTLD_NEXT,  "cudaMemset");
-	bypass->__cudaRegisterFatBinary    =  (fn__cudaRegisterFatBinary)  	dlsym(RTLD_NEXT,  "__cudaRegisterFatBinary");
-	bypass->__cudaRegisterFunction     =  (fn__cudaRegisterFunction)   	dlsym(RTLD_NEXT,  "__cudaRegisterFunction");
-	bypass->__cudaRegisterTexture      =  (fn__cudaRegisterTexture)    	dlsym(RTLD_NEXT,  "__cudaRegisterTexture");
-	bypass->__cudaRegisterVar          =  (fn__cudaRegisterVar)        	dlsym(RTLD_NEXT,  "__cudaRegisterVar");
-	bypass->cudaRuntimeGetVersion      =  (fn_cudaRuntimeGetVersion)   	dlsym(RTLD_NEXT,  "cudaRuntimeGetVersion");
-	bypass->cudaSetDevice              =  (fn_cudaSetDevice)           	dlsym(RTLD_NEXT,  "cudaSetDevice");
-	bypass->cudaSetDeviceFlags         =  (fn_cudaSetDeviceFlags)      	dlsym(RTLD_NEXT,  "cudaSetDeviceFlags");
-	bypass->cudaSetupArgument          =  (fn_cudaSetupArgument)       	dlsym(RTLD_NEXT,  "cudaSetupArgument");
-	bypass->cudaSetValidDevices        =  (fn_cudaSetValidDevices)     	dlsym(RTLD_NEXT,  "cudaSetValidDevices");
-	bypass->cudaStreamCreate           =  (fn_cudaStreamCreate)        	dlsym(RTLD_NEXT,  "cudaStreamCreate");
-	bypass->cudaStreamDestroy          =  (fn_cudaStreamDestroy)       	dlsym(RTLD_NEXT,  "cudaStreamDestroy");
-	bypass->cudaStreamQuery            =  (fn_cudaStreamQuery)         	dlsym(RTLD_NEXT,  "cudaStreamQuery");
-	bypass->cudaStreamSynchronize      =  (fn_cudaStreamSynchronize)   	dlsym(RTLD_NEXT,  "cudaStreamSynchronize");
-	bypass->cudaThreadExit             =  (fn_cudaThreadExit)          	dlsym(RTLD_NEXT,  "cudaThreadExit");
-	bypass->cudaThreadSynchronize      =  (fn_cudaThreadSynchronize)   	dlsym(RTLD_NEXT,  "cudaThreadSynchronize");
-	bypass->__cudaUnregisterFatBinary  =  (fn__cudaUnregisterFatBinary)	dlsym(RTLD_NEXT,  "__cudaUnregisterFatBinary");
+    char *dlerr;
+    void *cuda_handle;
 
-	if (!bypass->__cudaRegisterFatBinary) {
-		fprintf(stderr, "Error: binary was not linked with libcudart. Relink code.\n");
-		exit(1);
-	}
+    dlerror(); // clear errors
+    cuda_handle = dlopen("libcudart.so", RTLD_NOW);
+    if ((dlerr = dlerror())) {
+        fprintf(stderr, ">> DL ERROR: '%s'\n", dlerr);
+        exit(1);
+    }
+
+	bypass->cudaBindTexture            =  dlsym(cuda_handle,  "cudaBindTexture");
+	bypass->cudaBindTextureToArray     =  dlsym(cuda_handle,  "cudaBindTextureToArray");
+	bypass->cudaConfigureCall          =  dlsym(cuda_handle,  "cudaConfigureCall");
+	bypass->cudaCreateChannelDesc      =  dlsym(cuda_handle,  "cudaCreateChannelDesc");
+	bypass->cudaDriverGetVersion       =  dlsym(cuda_handle,  "cudaDriverGetVersion");
+	bypass->cudaFreeArray              =  dlsym(cuda_handle,  "cudaFreeArray");
+	bypass->cudaFree                   =  dlsym(cuda_handle,  "cudaFree");
+	bypass->cudaFreeHost               =  dlsym(cuda_handle,  "cudaFreeHost");
+	bypass->cudaFuncGetAttributes      =  dlsym(cuda_handle,  "cudaFuncGetAttributes");
+	bypass->cudaGetDeviceCount         =  dlsym(cuda_handle,  "cudaGetDeviceCount");
+	bypass->cudaGetDevice              =  dlsym(cuda_handle,  "cudaGetDevice");
+	bypass->cudaGetDeviceProperties    =  dlsym(cuda_handle,  "cudaGetDeviceProperties");
+	bypass->cudaGetErrorString         =  dlsym(cuda_handle,  "cudaGetErrorString");
+	bypass->cudaGetLastError           =  dlsym(cuda_handle,  "cudaGetLastError");
+	bypass->cudaGetTextureReference    =  dlsym(cuda_handle,  "cudaGetTextureReference");
+	bypass->cudaHostAlloc              =  dlsym(cuda_handle,  "cudaHostAlloc");
+	bypass->cudaLaunch                 =  dlsym(cuda_handle,  "cudaLaunch");
+	bypass->cudaMallocArray            =  dlsym(cuda_handle,  "cudaMallocArray");
+	bypass->cudaMalloc                 =  dlsym(cuda_handle,  "cudaMalloc");
+	bypass->cudaMallocPitch            =  dlsym(cuda_handle,  "cudaMallocPitch");
+	bypass->cudaMemcpyAsync            =  dlsym(cuda_handle,  "cudaMemcpyAsync");
+	bypass->cudaMemcpy                 =  dlsym(cuda_handle,  "cudaMemcpy");
+	bypass->cudaMemcpyFromSymbol       =  dlsym(cuda_handle,  "cudaMemcpyFromSymbol");
+	bypass->cudaMemcpyToArray          =  dlsym(cuda_handle,  "cudaMemcpyToArray");
+	bypass->cudaMemcpyToSymbolAsync    =  dlsym(cuda_handle,  "cudaMemcpyToSymbolAsync");
+	bypass->cudaMemcpyToSymbol         =  dlsym(cuda_handle,  "cudaMemcpyToSymbol");
+	bypass->cudaMemGetInfo             =  dlsym(cuda_handle,  "cudaMemGetInfo");
+	bypass->cudaMemset                 =  dlsym(cuda_handle,  "cudaMemset");
+	bypass->__cudaRegisterFatBinary    =  dlsym(cuda_handle,  "__cudaRegisterFatBinary");
+	bypass->__cudaRegisterFunction     =  dlsym(cuda_handle,  "__cudaRegisterFunction");
+	bypass->__cudaRegisterTexture      =  dlsym(cuda_handle,  "__cudaRegisterTexture");
+	bypass->__cudaRegisterVar          =  dlsym(cuda_handle,  "__cudaRegisterVar");
+	bypass->cudaRuntimeGetVersion      =  dlsym(cuda_handle,  "cudaRuntimeGetVersion");
+	bypass->cudaSetDevice              =  dlsym(cuda_handle,  "cudaSetDevice");
+	bypass->cudaSetDeviceFlags         =  dlsym(cuda_handle,  "cudaSetDeviceFlags");
+	bypass->cudaSetupArgument          =  dlsym(cuda_handle,  "cudaSetupArgument");
+	bypass->cudaSetValidDevices        =  dlsym(cuda_handle,  "cudaSetValidDevices");
+	bypass->cudaStreamCreate           =  dlsym(cuda_handle,  "cudaStreamCreate");
+	bypass->cudaStreamDestroy          =  dlsym(cuda_handle,  "cudaStreamDestroy");
+	bypass->cudaStreamQuery            =  dlsym(cuda_handle,  "cudaStreamQuery");
+	bypass->cudaStreamSynchronize      =  dlsym(cuda_handle,  "cudaStreamSynchronize");
+	bypass->cudaThreadExit             =  dlsym(cuda_handle,  "cudaThreadExit");
+	bypass->cudaThreadSynchronize      =  dlsym(cuda_handle,  "cudaThreadSynchronize");
+	bypass->__cudaUnregisterFatBinary  =  dlsym(cuda_handle,  "__cudaUnregisterFatBinary");
+
+    BUG(!bypass->__cudaRegisterFatBinary);
 }
 
